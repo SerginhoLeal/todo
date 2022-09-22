e650091ccfc61b9d0bd3d1ba9f0e0683c18e3aec
RuanV2 <ruanv@speediomail.com>
2022-09-21 22:58:34 -0300

    update: Add files to pattern up the project

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -7,12 +7,5 @@ export default {
   },
   fonts: {
     small: Dimensions.get('screen').width * 0.15,
-    showDeviceConfig: () => {
-      return [
-        `Device: ${Platform.OS}`,
-        `Tamanho do celular: ${Dimensions.get('screen').width}`,
-        `Tamanho da fonte: ${Dimensions.get('screen').width * 0.15}`
-      ]
-    }
   }
 }
