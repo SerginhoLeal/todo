789148b371cea4d93bd8c14d8c3152ff984f5c05
SerginhoLeal <sergiojunioleal@hotmail.com>
2022-09-22 22:59:50 -0300

    update: adding new colors

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -2,8 +2,19 @@ import { Dimensions, Platform } from 'react-native'
 
 export default {
   colors: {
-    blue: '#1E6F9F',
-    white_primary: "#F2F2F2"
+    gray_100: '#F2F2F2',
+    gray_200: '#D9D9D9',
+    gray_300: '#808080',
+    gray_400: '#333333',
+    gray_500: '#262626',
+    gray_600: '#1A1A1A',
+    gray_700: '#0D0D0D',
+
+    blue: '#4EA8DE',
+    purple: '#8284FA',
+    danger: '#E25858',
+    blue_dark: '#1E6F9F',
+    purple_dark: '#5E60CE',
   },
   fonts: {
     small: Dimensions.get('screen').width * 0.15,
