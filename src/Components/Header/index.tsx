e4930ffc1794b3ff4eebcfbfba3dd82b43e49871
RuanDevJs <ruanelpidio@hotmail.com>
2022-09-24 00:37:26 -0300

    update: Added tasks to be added and removed

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -3,8 +3,28 @@ import * as Styles from "./styles";
 
 import Icon from "../../assets/Logo.png";
 import { AntDesign } from '@expo/vector-icons';
+import HeaderProps from '../../@types/Components/Header';
+import { useState } from 'react';
+import { Task } from '../../@types/Screens/Home';
 
-export default function Header() {
+export default function Header({ tasks, setTasks }: HeaderProps) {
+  const [taskName, setTaskName] = useState<string>('');
+
+  function handlePress(){
+    if(taskName.trim() === ''){
+      return alert('Por favor insira sua tarefa!')
+    }
+
+    const newTask: Task = {
+      name: taskName,
+      completed: false,
+      key: Math.random() * 9
+    }
+
+    setTasks(oldValue => [...oldValue, newTask]);
+    setTaskName('');
+  }
+
   return (
     <Styles.Container>
       <Styles.WrapImage>
@@ -18,9 +38,12 @@ export default function Header() {
           <Styles.Input
             placeholder="Adicione uma nova tarefa"
             placeholderTextColor="#808080"
+            onChangeText={(e: string) => setTaskName(e)}
+            name={taskName}
           />
           <Styles.Button
             activeOpacity={0.72}
+            onPress={handlePress}
           >
             <AntDesign name="pluscircleo" size={24} color="#f2f2f2" />
           </Styles.Button>
--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as Native from 'react-native';
 import * as Styles from './styles';
 
 import { Card } from '../../Components/Card';
 import Header from '../../Components/Header';
+import { Task } from '../../@types/Screens/Home';
 
 export default function Home(){
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  function handleRemmove(currentIndex: number){
+    setTasks(oldValue => oldValue.filter((item, index) => index !== currentIndex));
+  }
+
+  function handleComplete(currentIndex: number){
+  }
+
   return (
     <Styles.Container>
-      <Header />
+      <Header tasks={tasks} setTasks={setTasks} />
       <Card />
 
     </Styles.Container>
