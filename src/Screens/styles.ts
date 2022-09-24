852fa9687fd65d1a13274fe46915e533da5f87a0
SerginhoLeal <sergiojunioleal@hotmail.com>
2022-09-23 21:13:15 -0300

    adding: styles home

---
+++ src/Screens/styles.ts
@@ -0,0 +1,8 @@
+import styled, { css } from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  ${({ theme }) => css`
+    background-color: ${theme.colors.gray_200};
+  `};
+`;
