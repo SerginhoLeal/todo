ea1caf2a747e77a7cbfa6cd40a3cd6dc00a2b67d
SerginhoLeal <sergiojunioleal@hotmail.com>
2022-09-24 00:39:46 -0300

    remove

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -7,7 +7,6 @@ import { Card } from '../../Components/Card';
 export default function Home(){
   return (
     <Styles.Container>
-      <Native.View style={{ backgroundColor: '#000', width: '100%', height: 173 }} />
 
       <Card />
 
