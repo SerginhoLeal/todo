223921f3aee184033d5b08bcbebbe6abb34159eb
SerginhoLeal <sergiojunioleal@hotmail.com>
2022-09-23 21:32:38 -0300

    adding: adding background color

--- src/Screens/Home/styles.ts
+++ src/Screens/Home/styles.ts
@@ -3,6 +3,6 @@ import styled, { css } from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   ${({ theme }) => css`
-    background-color: ${theme.colors.gray_200};
+    background-color: ${theme.colors.gray_600};
   `};
 `;
