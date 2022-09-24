fa3663d37ca523cd876c76e4ae6f8c00f25d3349
SerginhoLeal <sergiojunioleal@hotmail.com>
2022-09-23 23:45:40 -0300

    adding: import card component

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import * as Native from 'react-native';
 import * as Styles from './styles';
+
+import { Card } from '../../Components/Card';
 
 export default function Home(){
   return (
     <Styles.Container>
+      <Native.View style={{ backgroundColor: '#000', width: '100%', height: 173 }} />
+
+      <Card />
 
     </Styles.Container>
   );
